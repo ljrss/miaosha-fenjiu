19fedbb05b9d9a74fcbad56ef9d96ee624722dfd
吕嘉荣 <lvjiarong@lvjiarongdeMBP.lan>
2021-01-23 11:24:19 +0800

    “everything”

---
+++ conf/db.ini.php
@@ -0,0 +1,13 @@
+<?php
+$config['db']['master'] = array(
+    'host' => '127.0.0.1:3306',
+    'user' => 'miaosha1',
+    'password' => '123456',
+    'dbname' => 'miaosha1',
+);
+$config['db']['slave'] = array(
+    'host' => '127.0.0.1:3306',
+    'user' => 'miaosah1',
+    'password' => '123456',
+    'dbname' => 'miaosha1',
+);
